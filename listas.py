f32ce21a817be8050b33ee97917a25bfcd1419e0
Ruben Ralda <rubenralda@gmail.com>
2022-12-19 18:10:52 -0600

    Entrega de premios

--- listas.py
+++ listas.py
@@ -1,5 +1,7 @@
 import xml.etree.ElementTree as eT
-import os, webbrowser
+import os
+import webbrowser
+
 
 class NodoJugador:
 
@@ -28,8 +30,8 @@ class ListaJugadores:
         total_mov = 0
         arbol = eT.parse("entrada.xml")
         jugadores = arbol.findall("./jugador")
-        for jugador in jugadores: #recorrer todos los jugadores para guardar
-            if total_mov > 10000: #si se paso el limite
+        for jugador in jugadores:  # recorrer todos los jugadores para guardar
+            if total_mov > 10000:  # si se paso el limite
                 print(
                     "\nAdvertencia: Se llego al limite de movimientos, participantes restantes eliminados.\n")
                 break
@@ -37,15 +39,18 @@ class ListaJugadores:
             edad = jugador.find("./datospersonales/edad").text
             movimientos = jugador.find("./movimientos").text
             tamaño = jugador.find("./tamaño").text
-            if not tamaño.isnumeric(): #si no es un numero entonces no se puede guardar
-                print("\nError: el tamaño no es correcto " + nombre + "\n") 
+            if not tamaño.isnumeric():  # si no es un numero entonces no se puede guardar
+                print("\nError: el tamaño no es correcto " + nombre + "\n")
                 continue
-            if int(tamaño) > 30 or int(tamaño) % 5 != 0: #si cumple con las dimensiones correctas
-                print("\nAdvertencia: " + nombre + " no cumple con los requisitos de la estructura, jugador no agregado.\n")
+            # si cumple con las dimensiones correctas
+            if int(tamaño) > 30 or int(tamaño) % 5 != 0:
+                print("\nAdvertencia: " + nombre +
+                      " no cumple con los requisitos de la estructura, jugador no agregado.\n")
                 continue
             figura = jugador.find("./figura").text
             if figura.lower() != "estrella de belen" and figura.lower() != "arbol de navidad" and figura.lower() != "regalo" and figura.lower() != "arbol" and figura.lower() != "estrella" and figura.lower() != "árbol":
-                print("\nAdvertencia: La figura " + figura + " no existe " + nombre + ", jugador no agregado.\n")
+                print("\nAdvertencia: La figura " + figura +
+                      " no existe " + nombre + ", jugador no agregado.\n")
                 continue
             if not movimientos.isnumeric():
                 print("\nError: La cantidad de movimientos no es correcta.\n")
@@ -53,7 +58,8 @@ class ListaJugadores:
             total_mov += int(movimientos)
             nuevo = NodoJugador(nombre, edad, movimientos,
                                 tamaño, figura, puzzle, solucion)
-            nuevo.punteo = self.calcular_punteo(int(tamaño), int(movimientos), figura)
+            nuevo.punteo = self.calcular_punteo(
+                int(tamaño), int(movimientos), figura)
             if self.primero == None:
                 self.primero = self.ultimo = nuevo
             else:
@@ -62,14 +68,15 @@ class ListaJugadores:
 
     def avanzar_jugador(self):
         if self.primero != None:
-            print("\n¡El " + self.primero.nombre + " jugador termino de jugar!\n")
+            print("\n¡El " + self.primero.nombre +
+                  " jugador termino de jugar!\n")
             self.primero = self.primero.siguiente
         else:
             print("\nAdvertencia: La cola esta vacia.\n")
 
-    def calcular_punteo(self,dimension : int, movimientos : int, figura : str):
+    def calcular_punteo(self, dimension: int, movimientos: int, figura: str):
         punteo = 0
-        for i in range(5,35,5):
+        for i in range(5, 35, 5):
             punteo += 25
             if dimension == i:
                 break
@@ -102,7 +109,8 @@ class ListaJugadores:
         while True:
             text += aux.nombre + str(aux.edad) + "[label=\"Nombre: " + aux.nombre + "\\nEdad: " + \
                 aux.edad+"\\nMovimientos: " + str(aux.movimientos)+"\\nTamaño: " + \
-                str(aux.tamaño) +"\\nFigura: "+ aux.figura + "\\nPunteo: " + str(aux.punteo) + "\"]\n"
+                str(aux.tamaño) + "\\nFigura: " + aux.figura + \
+                "\\nPunteo: " + str(aux.punteo) + "\"]\n"
             conexion += aux.nombre + str(aux.edad)
             aux = aux.siguiente
             if aux == None:
@@ -111,7 +119,7 @@ class ListaJugadores:
         text += conexion
         return text
 
-    def crearReporte(self):
+    def crear_reporte(self):
         contenido = "digraph G{\n\n"
         r = open("./textos/colaJugadores.txt", "w")
         contenido += str(self.report())
@@ -119,68 +127,142 @@ class ListaJugadores:
         r.write(contenido)
         r.close()
         if self.primero != None:
-            os.system("dot -Tpng -Gcharset=latin1 textos/colaJugadores.txt -o colaJugadores.png")
+            os.system(
+                "dot -Tpng -Gcharset=latin1 textos/colaJugadores.txt -o colaJugadores.png")
             webbrowser.open("colaJugadores.png")
+
 
 class ListaTop10:
 
     def __init__(self):
+        self.conteo = 0
         self.primero = None
         self.ultimo = None
 
     def agregar_jugador(self, nuevo: NodoJugador):
         if nuevo == None:
+            print("La cola esta vacia.")
             return
         nuevo.siguiente = None
-        if self.primero == None:
+        if self.ultimo == None:
             self.primero = self.ultimo = nuevo
+            self.conteo += 1
         else:
             aux = self.primero
-            if nuevo.punteo < self.primero.punteo:
+            if nuevo.punteo > self.primero.punteo:
                 nuevo.siguiente = self.primero
                 self.primero.anterior = nuevo
                 self.primero = nuevo
+                self.conteo += 1
+                if self.conteo > 10:
+                    self.ultimo = self.ultimo.anterior
+                    self.ultimo.siguiente = None
+                    self.conteo = 10
                 return
-            for i in range(10):
-                if nuevo.punteo >= aux.punteo:
+            while True:
+                if nuevo.punteo <= aux.punteo:
                     if aux.siguiente == None:
                         aux.siguiente = nuevo
                         self.ultimo = nuevo
                         self.ultimo.anterior = aux
+                        self.conteo += 1
+                        if self.conteo > 10:
+                            self.ultimo = self.ultimo.anterior
+                            self.ultimo.siguiente = None
+                            self.conteo = 10
                         break
-                    elif nuevo.punteo <= aux.siguiente.punteo:
+                    elif nuevo.punteo >= aux.siguiente.punteo:
                         nuevo.siguiente = aux.siguiente
                         aux.siguiente.anterior = nuevo
                         nuevo.anterior = aux
                         aux.siguiente = nuevo
+                        self.conteo += 1
+                        if self.conteo > 10:
+                            self.ultimo = self.ultimo.anterior
+                            self.ultimo.siguiente = None
+                            self.conteo = 10
                         break
                     aux = aux.siguiente
 
-    def imprimirTop10(self):
-        aux = self.ultimo
-        for i in range(1,11):
+    def imprimir_top10(self):
+        if self.ultimo != None:
+            aux = self.primero
+        else:
+            aux = None
+        for i in range(1, 11):
             if aux != None:
-                print("\n"+str(i) + ". " + aux.nombre + " Punteo: " + str(aux.punteo))
-                aux = aux.anterior
+                print(str(i) + ". " + aux.nombre +
+                      " Punteo: " + str(aux.punteo))
+                aux = aux.siguiente
             else:
                 print(str(i) + ".")
-        print("\n")
+
+    def vaciar_lista(self):
+        self.conteo = 0
+        self.primero = None
+        self.ultimo = None
+    
+    def eliminar_ultimo(self):
+        if self.ultimo == None:
+            print("No hay Top 10")
+            return None
+        if self.ultimo.anterior != None:
+            self.ultimo = self.ultimo.anterior
+            aux = self.ultimo.siguiente
+            self.ultimo.siguiente = None
+            self.conteo -= 1
+            return aux
+        else:
+            self.conteo = 0
+            self.ultimo = None
+            print("Ultimo premio entregado")
+            return self.primero
+
+
+class Premios:
+
+    def __init__(self, lugar, regalo) -> None:
+        self.lugar = lugar
+        self.regalo = regalo
+        self.siguiente: Premios = None
+        self.anterior: Premios = None
+
+
+class PilaPremios:
+
+    def __init__(self):
+        self.primero = None
+        self.ultimo = None
+
+    def agregar_premios(self):
+        self.primero = self.ultimo = None
+        arbol = eT.parse("premios.xml")
+        premios = arbol.findall("./premio")
+        for premio in premios:  # recorrer todos los premios para guardar
+            lugar = premio.find("./lugar").text
+            regalo = premio.find("./regalo").text
+            nuevo = Premios(lugar, regalo)
+            if self.ultimo == None:
+                self.primero = self.ultimo = nuevo
+            else:
+                self.ultimo.siguiente = nuevo
+                nuevo.anterior = self.ultimo
+                self.ultimo = nuevo
 
     def report(self):
-        if self.primero == None:
+        if self.ultimo == None:
             return ""
         aux = self.primero
         conexion = ""
         text = ""
-        text += "rankdir=LR; \n "
-        text += "node[shape=component, style=filled, color=skyBlue, fontname=\"Century Gothic\"]; \n "
+        text += "rankdir=TB; \n "
+        text += "node[shape=cds, style=filled, color=pink, fontname=\"Century Gothic\"]; \n "
         text += "graph [fontname=\"Century Gothic\"]; \n "
-        text += "labelloc=\"t\"; label=\"Cola de jugadores\"; \n"
+        text += "labelloc=\"t\"; label=\"Pila de premios\"; \n"
         while True:
-            text += aux.nombre + str(aux.edad) + "[label=\"Nombre: " + aux.nombre + "\\nEdad: " + \
-                aux.edad+"\\nMovimientos: " + str(aux.movimientos)+"\\nTamaño: " + \
-                str(aux.tamaño) +"\\nFigura: "+ aux.figura + "\\nPunteo: " + str(aux.punteo) + "\"]\n"
-            conexion += aux.nombre + str(aux.edad)
+            text += aux.lugar + "[label=\"Lugar: " + \
+                aux.lugar + "\\nPremio: " + aux.regalo + "\"]\n"
+            conexion += aux.lugar
             aux = aux.siguiente
             if aux == None:
                 break
@@ -188,18 +270,25 @@ class ListaTop10:
         text += conexion
         return text
 
-    def crearReporte(self):
+    def crear_reporte(self):
         contenido = "digraph G{\n\n"
-        r = open("./textos/top.txt", "w")
+        r = open("./textos/pilaPremios.txt", "w")
         contenido += str(self.report())
         contenido += "\n}"
         r.write(contenido)
         r.close()
         if self.primero != None:
-            os.system("dot -Tpng -Gcharset=latin1 textos/top.txt -o top.png")
-            webbrowser.open("top.png")
+            os.system(
+                "dot -Tpng -Gcharset=latin1 textos/pilaPremios.txt -o pilaPremios.png")
+            webbrowser.open("pilaPremios.png")
 
-class PilaPremios:
-
-    def __init__(self) -> None:
-        pass
+    def eliminar_premio(self):
+        if self.ultimo == None:
+            return None
+        else:
+            if self.primero.siguiente == None:
+                self.ultimo = None
+                return self.primero 
+            self.primero = self.primero.siguiente
+            aux = self.primero.anterior
+            return aux
--- main.py
+++ main.py
@@ -1,29 +1,57 @@
 from listas import *
 
-listaJugadores = ListaJugadores()
-lista_top_10 = ListaTop10()
+lista_jugadores = ListaJugadores()
+lista_top10 = ListaTop10()
+premios = PilaPremios()
 opcion = ""
+vacio = False
+cargado = False
+jugador = None
 while opcion != "8":
     print("-----------------------------Menu---------------------------------")
     print("1. Cargar archivo de configuracion")
-    print("2. Avanzar jugador")
-    print("3. Simular todos los jugadores")
+    print("2. Avanzar jugador en la cola")
+    print("3. Simular todos los juegos")
     print("4. Ver figura del proximo jugador")
     print("5. Ver Top 10 jugadores")
     print("6. Ver pila de premios")
-    print("7. Iniciar premiación")
+    print("7. Dar premio")
     print("8. Salir")
     opcion = input("Escribe el numero de la opcion:> ")
     match opcion:
         case "1":
-            listaJugadores.agregar_jugadores()
-            listaJugadores.crearReporte()
+            lista_jugadores.agregar_jugadores()
+            lista_top10.vaciar_lista()
+            lista_jugadores.crear_reporte()
+            vacio = False
         case "2":
-            aux = listaJugadores.primero
-            listaJugadores.avanzar_jugador()
-            lista_top_10.agregar_jugador(aux)
-            listaJugadores.crearReporte()
+            aux = lista_jugadores.primero
+            lista_jugadores.avanzar_jugador()
+            lista_top10.agregar_jugador(aux)
+            lista_jugadores.crear_reporte()
+            if aux == None:
+                vacio = True
+        case "3":
+            aux = lista_jugadores.primero
+            while aux != None:
+                lista_jugadores.avanzar_jugador()
+                lista_top10.agregar_jugador(aux)
+                aux = lista_jugadores.primero
+            vacio = True
+            print("\nLa cola ha sido vaciada.\n")
         case "5":
-            lista_top_10.imprimirTop10()
+            print("\n")
+            lista_top10.imprimir_top10()
+            print("\n")
+        case "6":
+            premios.agregar_premios()
+            premios.crear_reporte()
+            cargado = True
+        case "7":
+            jugador = lista_top10.eliminar_ultimo()
+            regalo = premios.eliminar_premio()
+            if jugador != None and regalo != None and vacio == True:
+                premios.crear_reporte()
+                print("Entrega a " + jugador.nombre + " el premio " + regalo.regalo + " por el " + regalo.lugar + " lugar.")
         case "8":
             exit()
